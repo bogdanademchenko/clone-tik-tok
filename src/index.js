b0b9f01409b882986ecacac4edefe4de76dc896c
Bogdana <demchenkobb@gmail.com>
2023-02-02 22:02:41 +0200

    router

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { HashRouter, Route, Switch } from 'react-router-dom';
 import './index.css';
 import App from './App';
 

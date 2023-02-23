964d65683f62eaa1bb68de9b5499164d0309423c
Bogdana <demchenkobb@gmail.com>
2023-02-23 21:30:10 +0200

    fix

--- src/index.js
+++ src/index.js
@@ -1,8 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { HashRouter, Route, Switch } from 'react-router-dom';
-import './index.css';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { HashRouter, Route, Switch } from 'react-router-dom'
+import Header from './components/Header'
+import Home from './pages/Home'
+import Upload from './pages/Upload'
+
+import './App.css'
+
+
+
+const App = () => {
+  return (
+    <HashRouter>
+      <Header/>
+      <Switch>
+         <Route path='/upload' component={Upload}/>
+        <Route path='/' component={Home}/>
+      </Switch>
+    </HashRouter>
+  );
+}
+
+export default App
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
